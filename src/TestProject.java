a54dc627cdd9f4bcf202c7c2ddce0c27a1730a1f
sbt-kazakov-an <Kazakov.A.N@sberbank.ru>
2017-11-28 17:36:34 +0300

    commit_281117_1736

--- src/TestProject.java
+++ src/TestProject.java
@@ -4,7 +4,5 @@ public class TestProject{
         System.out.println("Hello World!!!");
         System.out.println("Hello World!!!");
         System.out.println("Hello World!!!");
-        System.out.println("Hello World!!!");
-        System.out.println("Hello World!!!");
     }
 }
