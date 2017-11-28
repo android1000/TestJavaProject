56525623bca798ee646e952e6f6c866efa994f03
sbt-kazakov-an <Kazakov.A.N@sberbank.ru>
2017-11-28 19:05:19 +0300

    commit_281117_1905

--- src/NewPackage/InputClass.java
+++ src/NewPackage/InputClass.java
@@ -1,15 +1,19 @@
 package NewPackage;
 
 import java.util.Scanner;
+import java.lang.Math;
 
 public class InputClass {
     public static void main(String[] args){
         int a,b,c;
+        double d;
         String s;
         a=new Scanner(System.in).nextInt();
         s=new Scanner(System.in).next();
         b=a+5;
         System.out.println(b);
+        d=Math.sqrt(b);
+        System.out.println(d);
         System.out.println(s);
     }
 
